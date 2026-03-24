c87a689630ad4e2863d29ec3acd262f413ae82b5
Amanda Guerra Moontenegro <65723434+amandulis8@users.noreply.github.com>
2026-03-24 16:07:38 -0400

    Add Fuse.js  for search feature
    Implement an example from Fuse.js for search functionality in search bar.

--- search-bar-amanda/search-bar.js
+++ search-bar-amanda/search-bar.js
@@ -1,1 +1,16 @@
+const Fuse = require('fuse.js');
 
+const fuseOptions = {
+
+	keys: [
+		"title",
+		"author.firstName"
+	]
+};
+
+const fuse = new Fuse(list, fuseOptions);
+
+
+const searchPattern = ""
+
+return fuse.search(searchPattern)
