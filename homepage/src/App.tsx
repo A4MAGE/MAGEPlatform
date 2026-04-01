b842329ce4ecd9c3eb58c318a22ae03bc7626a03
ronidomi <roni.domi001@umb.edu>
2026-04-01 17:41:49 -0400

    import fix

--- homepage/src/App.tsx
+++ homepage/src/App.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home.tsx'
 import Explore from './pages/Explore.tsx'
-import About from './pages/Engine.tsx'
+import Engine from './pages/Engine.tsx'
 import Navbar from './components/Navbar.tsx'
 
 function App() {
--- homepage/src/pages/Engine.tsx
+++ homepage/src/pages/Engine.tsx
@@ -1,6 +1,9 @@
+import "../App.css"
+
 function Engine() {
   return (
     <>
+      <h1>h1</h1>
     </>
   )
 }
